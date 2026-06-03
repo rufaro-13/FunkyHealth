bd7fcd4044df337af65275b16416fee43414e2df
rufaro-13 <rufarochiuta@gmail.com>
2026-06-04 01:59:57 +0200

    activites added

--- funkycareltd/src/App1.jsx
+++ funkycareltd/src/App1.jsx
@@ -4,6 +4,7 @@ import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
+import Activities from "./pages/Activities";
 import {Routes, Route } from 'react-router-dom';
 /* import Tables from "./components/Tables"; */
 export default function App1() {
@@ -16,6 +17,7 @@ export default function App1() {
                <Route forceRefresh={true} index element = {<Home/>}></Route>
                <Route forceRefresh={true} path='/about' element= {<About/>}></Route>
                <Route forceRefresh={true} path='/services' element= {<Services/>}></Route>
+               <Route forceRefresh={true} path='/activities' element= {<Activities/>}></Route>
             </Route>
       </Routes>
 
